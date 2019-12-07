a5d55ad262ba879fe0d57825c32cbc5cd11089e5
daurnimator <quae@daurnimator.com>
2019-12-08 01:19:10 +1100

    std: add http.isTokenChar

--- lib/std/http.zig
+++ lib/std/http.zig
@@ -3,3 +3,17 @@ test "std.http" {
 }
 
 pub const Headers = @import("http/headers.zig").Headers;
+
+/// Valid token characters
+const tchars = blk: {
+    var t = [_]bool{false} ** 128;
+    for ("!#$%&'*+-.^_`|~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") |c| {
+        t[c] = true;
+    }
+    break :blk t;
+};
+
+pub fn isTokenChar(c: u8) bool {
+    if (c > 127) return false;
+    return tchars[c];
+}
