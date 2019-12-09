daurnimator/zig

commit 94485b2a58f8efbfb61e46d2efa257420f12a22d

std: clean up fifo.readableSliceMut

lib/std/fifo.zig
@@ -145,16 +145,13 @@ pub fn LinearFifo(
         fn readableSliceMut(self: SliceSelfArg, offset: usize) []T {
             if (offset > self.count) return [_]T{};
 
-            const start = self.head + offset;
+            var start = self.head + offset;
             if (start >= self.buf.len) {
-                return self.buf[start - self.buf.len ..][0 .. self.count - offset];
+                start -= self.buf.len;
+                return self.buf[start..self.count - offset];
             } else {
-                const end: usize = self.head + self.count;
-                if (end >= self.buf.len) {
-                    return self.buf[start..self.buf.len];
-                } else {
-                    return self.buf[start..end];
-                }
+                const end = math.min(self.head + self.count, self.buf.len);
+                return self.buf[start..end];
             }
         }
 

commit 2545375e69459456bab4220608d0bc628ab18c57

std: fix bug in fifo.readableSliceMut
Introduced in 94485b2a58f8efbfb61e46d2efa257420f12a22d

lib/std/fifo.zig
@@ -146,13 +146,14 @@ pub fn LinearFifo(
             if (offset > self.count) return &[_]T{};
 
             var start = self.head + offset;
+            var end: usize = undefined;
             if (start >= self.buf.len) {
                 start -= self.buf.len;
-                return self.buf[start .. self.count - offset];
+                end = self.count - offset;
             } else {
-                const end = math.min(self.head + self.count, self.buf.len);
-                return self.buf[start..end];
+                end = math.min(self.count, self.buf.len - self.head) - offset;
             }
+            return self.buf[start..][0..end];
         }
 
         /// Returns a readable slice from `offset`